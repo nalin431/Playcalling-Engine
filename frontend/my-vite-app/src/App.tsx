51fe5d63ba04feb68578659761022c529cefb5c6
Nalin <nalinkundu@gmail.com>
2026-01-22 13:22:19 -0600

    Expanding front page

--- frontend/my-vite-app/src/App.tsx
+++ frontend/my-vite-app/src/App.tsx
@@ -45,9 +45,22 @@ function App() {
                     <p className="hero-blurb">
                       This project recommends play categories based on game situation, opponent context, and
                       model-based outcomes. It is designed to surface the best call and explain why it wins.
+
+                      How it works
+                      The engine utilizes two  models, one geared towards calling a sucessful play (as defined below) and 
+                      one that maximizes 
+
+                      Limitations
+                      Unfortunetly, actual playcalls are propietary to NFL teams. This engine utilizes data from PFF and ESPN, sources 
+                      which don't include data on formation, play action or screen passes. A new version is being worked on that includes
+                      data from other sources, but this version, for now, focuses on the best concept of play in a given situation. 
+
+                      
+
+
                     </p>
                     <div className="hero-definition">
-                      <h3>How We Define Success</h3>
+                      <h3>How Sucess is Defined</h3>
                       <ul>
                         <li>1st down: gain at least 40% of yards to go</li>
                         <li>2nd down: gain at least 60% of yards to go</li>
@@ -61,6 +74,7 @@ function App() {
                 </section>
               }
             />
+            
             <Route
               path="/engine"
               element={
