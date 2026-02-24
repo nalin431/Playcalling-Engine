e8a44c84ff0911b942826da923e6f42d583e14af
Nalin <nalinkundu@gmail.com>
2026-02-24 16:33:58 -0600

    Action returns health response body

--- HealthCheck/ping.py
+++ HealthCheck/ping.py
@@ -10,6 +10,6 @@ else:
     try:
         health = requests.get(url, timeout = (10,10))
         health.raise_for_status()
-        print("Status: ", health)
+        print("Status: ", health.json())
     except HTTPError as http_err:
         print(f"HTTP error occurred: {http_err}")
